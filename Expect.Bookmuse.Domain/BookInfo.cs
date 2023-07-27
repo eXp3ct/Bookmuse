7248622eb2ced04170a2c74771997cb7b48fde6a
Рома Веселов <MWoShka@outlook.com>
2023-07-27 18:23:05 +0300

    #7 bookinfo model

---
+++ Expect.Bookmuse.Domain/BookInfo.cs
@@ -0,0 +1,15 @@
+﻿using Expect.Bookmuse.Domain.Enums;
+
+namespace Expect.Bookmuse.Domain
+{
+	public class BookInfo
+	{
+		public string? Name { get; set; }
+		public DateOnly? ReleaseDate { get; set; }
+		public List<Genre>? Genres { get; set; }
+		public string? Volume { get; set; }
+		public string? Publisher { get; set; }
+		public int? NumberOfPages { get; set; }
+		public decimal? Price { get; set; }
+	}
+}
